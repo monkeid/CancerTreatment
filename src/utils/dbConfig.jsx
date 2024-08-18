1cdd3e4799d2b8fdf7d60a401021cf1a1c405d23
monkeid <jackoraro0@gmail.com>
2024-08-18 11:40:34 +0300

    updates

--- src/utils/dbConfig.jsx
+++ src/utils/dbConfig.jsx
@@ -2,6 +2,6 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 const sql = neon(
-  "postgresql://finan-smart_owner:uk3aed9QZotj@ep-wispy-breeze-a5iadk8t.us-east-2.aws.neon.tech/beat-cancer?sslmode=require"
+  "postgresql://cancertreatment_owner:7by2eLYtMOrl@ep-calm-star-a5fk2lru.us-east-2.aws.neon.tech/cancertreatment?sslmode=require"
 );
 export const db = drizzle(sql, { schema });
